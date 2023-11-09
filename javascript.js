b1e5ac32b8be2c033cea6bcad9affae055ce696b
finiavana <miandryfiniavana@gmail.com>
2023-11-09 12:15:09 +0300

    add exiting file

---
+++ javascript.js
@@ -0,0 +1,21 @@
+var btn = document.getElementById("button");
+var affichageResult = document.getElementById("affichageResult");
+btn.onclick = function(){
+    var start = document.getElementById("start").value;
+    var end = document.getElementById("end").value;
+    var ilayNombre = document.getElementById("ilayNombre").value;
+    affichageResult.innerText = ' ';
+    affichageResult.style.backgroundColor = "rgba(128, 128, 128, 0.637)";
+    affichageResult.style.width = "80%";
+    affichageResult.style.margin = "auto";
+    affichageResult.style.marginTop = "30px";
+    affichageResult.style.padding = "20px";
+    affichageResult.style.textAlign = "center";
+    affichageResult.style.borderRadius = "30px";
+    affichageResult.innerText = "ireo nombre divisible par"+" "+ilayNombre+" "+"dia\n";
+    for(var i = start; i <= end; i++){
+        if(i%ilayNombre==0){
+            affichageResult.innerText = affichageResult.innerText + i + "\n";
+        }
+        } 
+}
